3482927ad4eba1937745871d798988f18306a042
Roman Le Négrate <roman.lenegrate@gmail.com>
2014-12-17 21:00:12 +0100

    examples/server_accept_manual: don't raise

--- examples/server_accept_manual.rb
+++ examples/server_accept_manual.rb
@@ -1,14 +1,17 @@
 require 'nbio'
 require 'socket'
 
+host, port = 'localhost', 1234
+
 NBIO::Loop.run do |lo|
-  server = TCPServer.new('localhost', 1234)
+  server = TCPServer.new(host, port)
+  puts "Listening on #{host}:#{port}"
   accept_next = lambda do
     begin
       sock = server.accept_nonblock
     rescue IO::WaitReadable
       lo.monitor_read(server).
-        catch { |err| raise err }.
+        catch { |err| p err: err }.
         then { accept_next.call }
     else
       accept_next.call
