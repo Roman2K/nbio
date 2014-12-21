fac25a257378023c336b9f2c7c47e44e3818817d
Roman Le Négrate <roman.lenegrate@gmail.com>
2014-12-21 11:54:12 +0100

    fix that Acceptor could accept in the current tick

--- lib/nbio/acceptor.rb
+++ lib/nbio/acceptor.rb
@@ -4,23 +4,25 @@ module NBIO
       @lo = lo
       @sock = sock
       @ev = EventEmitter.new
-      accept_next
+      monitor_next
     end
 
     attr_reader :ev
 
   private
 
-    def accept_next
-      sock = @sock.accept_nonblock
-    rescue IO::WaitReadable
+    def monitor_next
       @lo.monitor_read(@sock).
         catch { |err| @ev.emit(:err, err) }.
-        then { accept_next }
+        then { handle_accept_ready }
+    end
+
+    def handle_accept_ready
+      sock = @sock.accept_nonblock
     rescue SystemCallError
       @ev.emit(:err, $!)
     else
-      accept_next
+      monitor_next
       @ev.emit(:conn, sock)
     end
   end
